3dc4e1a5401dd3787762c4930c83b8bd5e7a049b
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-07-20 13:42:14 -0300

    basic `SpotifyAPI` request + `RYMdata.txt` scrapper script

--- album-scrapper-spotifyAPI.py
+++ album-scrapper-spotifyAPI.py
@@ -4,26 +4,30 @@ import env
 def GetApiToken():
     authentication = f'grant_type=client_credentials&client_id={env.clientId}&client_secret={env.clientSecret}'
     rawTokenInfo = requests.post(url='https://accounts.spotify.com/api/token', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=authentication)
-    accessToken = rawTokenInfo["access_token"]
-    tokenType = rawTokenInfo["token_type"]
-    return {accessToken, tokenType}
+    TokenInfo = rawTokenInfo.json()
+    accessToken = TokenInfo['access_token']
+    tokenType = TokenInfo['token_type']
+    return (accessToken, tokenType)
+
+def ApiQuery(album, artist, headers):
+    parsedAlbum = requests.utils.quote(album)
+    parsedArtist = requests.utils.quote(artist)
+    url = 'https://api.spotify.com/v1/search?'
+    query = f'q=album:{parsedAlbum}%20artist:{parsedArtist}&type=album'
+    response = requests.get(url=url+query, headers=headers)
+    return response.text
 
 run = True
 
 if run:
     data = GetApiToken()
-    print(data)
+    print('access_token:', data[0], '\ntokenType:', data[1])
+    headers = {'Authorization': f'{data[1]}  {data[0]}'}
 
-
-url = 'https://api.spotify.com/v1/search?'
-headers = {'Authorization': f'{data.tokenType} {data.accessToken}'}
 
 with open('RYMdata.txt', 'r') as arquivo:
-    for i in arquivo:
-        print(i)
-        album = ''
-        artist = ''
-        query = f'q=album:{album}%20artist:{artist}&type=album'
-        response = requests.get(url=url+query, headers=headers)
-        with open('parsedRYMdata.txt', 'w') as escritor:
-            escritor.write(response.text)
+    for query in arquivo:
+        (album, artist) = query.split(' - ')
+        print('album:', album, '----->', artist)
+        rawData = ApiQuery(album=album, artist=artist, headers=headers)
+        print(rawData)
