09e6deed3e633a6d5c58094838b48157edc720da
Lucas Guido <lucasguidocp@gmail.com>
2024-07-21 15:35:48 -0300

    script now collects all info from the chart

--- rym_scraper.py
+++ rym_scraper.py
@@ -21,22 +21,45 @@ with open("rym_list.csv", "w", newline='', encoding="utf-8") as csvfile:
             while pos <= i*40:
                 entry = chart.find('div', {"id": f"pos{pos}"}).find('div',
                                 {"class": "page_charts_section_charts_item_info"})
-                top_line = entry.find('div', {"class": "page_charts_section_charts_top_line"}).find(
+                top_line = entry.find('div', {"class": "page_charts_section_charts_top_line"})
+                top_line_title = top_line.find(
                            'div', {"class": "page_charts_section_charts_top_line_title_artist"})
-                album = top_line.find('div', {"class":
+                album = top_line_title.find('div', {"class":
                         "page_charts_section_charts_item_title"}).a.span.span.text                
-                artist_div = top_line.find('div', {"class":
+                artist = []
+                artist_div = top_line_title.find('div', {"class":
                         "page_charts_section_charts_item_credited_links_primary"})
                 if artist_div.a is None:
-                    artist = "Various"
+                    artist = ["Various"]
                 else:
-                    artist_span = artist_div.a.span
-                    if artist_span.span is None: # TODO: more than one artist
-                        artist = artist_span.text.lstrip()
-                    else:
-                        artist = artist_span.span.text.lstrip()
+                    artist_span = [e.span for e in artist_div.find_all('a') if e.span is not None]
+                    for span in artist_span:
+                        if span.span is None: # TODO: more than one artist
+                            artist.append("!")
+                            artist.append(f"{span.text.lstrip()}")
+                        else:
+                            artist.append(span.span.text.lstrip())
                 date = entry.find('div',{"class": "page_charts_section_charts_item_date"}).span.text
                 year = date[-4:]
-                print(f"{pos} - {artist} - {album} ({year})")
-                listwriter.writerow([pos, artist, album, year])
+                genre_div = entry.find('div',
+                            {"class": "page_charts_section_charts_item_genres_primary"})
+                genre = [e.text for e in genre_div.find_all('a') if e.text is not None]
+                second_div = entry.find('div',
+                            {"class": "page_charts_section_charts_item_genres_secondary"})
+                if second_div is None:
+                    second_genre = [""]
+                else:
+                    second_genre = [e.text for e in second_div.find_all('a') if e.text is not None]
+                descriptor_div = entry.find('div',
+                                 {"class": "page_charts_section_charts_item_genre_descriptors"})
+                descriptor = [e.text for e in descriptor_div.find_all('span') if e.text is not None]
+                stats_div = top_line.find('div', {"class": "page_charts_section_charts_item_stats"})
+                average = stats_div.find('span',
+                          {"class": "page_charts_section_charts_item_details_average_num"}).text
+                ratings = stats_div.find('span', {"class":
+                                         "page_charts_section_charts_item_details_ratings"}).find(
+                                         'span', {"class": "full"}).text.strip().replace(",", "")
+                print(f"{pos} - {artist}: {album} ({average} - {ratings})")
+                listwriter.writerow([pos, artist, album, year, genre, second_genre,
+                                     descriptor, average, ratings])
                 pos += 1
