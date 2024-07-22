4971496923bcad6ff11d8cb019cd50e4dac3a391
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-07-21 22:49:18 -0300

    adds `decade_count.csv`

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 
 def make_array(text):
+    text = str(text)
     text = text.strip("[]")
     resulting_array = text.split(", ")
     for i in range(len(resulting_array)):
@@ -18,6 +19,26 @@ def get_count(df, data_type):
         line = make_array(line)
         for element in line:
             df_list.append(element)
+
+    data = {
+        f"{data_type}": df_list
+    }
+
+    new_df = pd.DataFrame(data)
+    count = pd.crosstab(new_df[f"{data_type}"], "appearances")
+    return count
+
+def get_decade_count(df, data_type):
+
+    def get_decade(year):
+        return (int(year)//10)*10
+    
+    df_list = []
+    for line in df:
+        line = make_array(line)
+        for element in line:
+            decade = get_decade(element)
+            df_list.append(decade)
 
     data = {
         f"{data_type}": df_list
@@ -40,3 +61,7 @@ print(second_genre_count)
 
 descriptor_count = get_count(data["descriptor"], "descriptor")
 descriptor_count.to_csv('descriptor_count.csv')
+
+decade_count = get_decade_count(data["year"], "year")
+decade_count.to_csv('decade_count.csv')
+
