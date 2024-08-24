e52d2277a2013ad0c5b7e379f62bf1ff1b88734b
Lucas Guido <lucasguidocp@gmail.com>
2024-08-24 15:05:13 -0300

    most metrics working

--- scripts/graph.py
+++ scripts/graph.py
@@ -13,8 +13,8 @@ mean = graph_connections['Weight'].mean()
 print('Weight mean: ', mean)
 graph_connections_filtered = graph_connections[graph_connections['Weight'] > mean]
 graph_connections_filtered.to_csv(os.path.join(root_dir, 'data', 'graph', 'graph_connections_filtered.csv'), index=False)
-filepath_new = os.path.join(root_dir, 'data', 'graph', 'graph_connections_filtered.csv')
-data = open(filepath_new, "r")
+vis_filepath = os.path.join(root_dir, 'data', 'graph', 'graph_connections_filtered.csv')
+data = open(filepath, "r")
 
 next(data, None)
 Graphtype = nx.Graph()
@@ -29,23 +29,19 @@ def calculate_metrics():
     degree_centrality = G.degree(weight='weight')
     average_path_length = nx.average_shortest_path_length(G, weight='reverse_weight') # also add without weight
     average_path_length_unweighted = nx.average_shortest_path_length(G)
-    closeness_centrality = nx.closeness_centrality(G)
-    betweenness_centrality = nx.betweenness_centrality(G, weight='reverse_weight')
-    communities = nx.community.louvain_communities(G, weight='weight', resolution=2)
+    # closeness_centrality = nx.closeness_centrality(G)
+    # betweenness_centrality = nx.betweenness_centrality(G, weight='reverse_weight')
+    communities = nx.community.louvain_communities(G, weight='weight', resolution=3, seed=72)
     triangles = nx.triangles(G)
     clustering = nx.clustering(G, weight='weight')
     average_clustering = nx.average_clustering(G, weight='weight')
 
-    print(f"Average path length: {average_path_length}")
-    print(f"Unweighted path length: {average_path_length_unweighted}")
     
-    communities_filename = os.path.join(root_dir, '.\data\graph\general_metrics.txt')
-    with open(communities_filename, 'w', encoding='utf-8') as f:
+    filename = os.path.join(root_dir, 'data', 'graph', 'general_metrics.txt')
+    with open(filename, 'w', encoding='utf-8') as f:
+        print(f"Average path length: {average_path_length}", end='\n\n', file=f)
+        print(f"Unweighted average path length: {average_path_length_unweighted}", end='\n\n', file=f)
         print(f"Average clustering: {average_clustering}", end='\n\n', file=f)
-        print("Communities:", file=f)
-        for index, community in enumerate(communities):
-            print(index, file=f)
-            print(get_info_by_node(community, ['genre', 'second_genre', 'artist','descriptor']), end='\n\n', file=f)
 
     node_to_community = dict()
     for index, community in enumerate(communities):
@@ -57,34 +53,36 @@ def calculate_metrics():
     sorted_degrees = sorted(degree_centrality)
     data_dict['degree'] = [degree for _, degree in sorted_degrees]
 
-    data_dict['closeness'] = to_vector(closeness_centrality)
-    data_dict['betweenness'] = to_vector(betweenness_centrality)
+    # data_dict['closeness'] = to_vector(closeness_centrality)
+    # data_dict['betweenness'] = to_vector(betweenness_centrality)
     data_dict['community'] = to_vector(node_to_community)
     data_dict['triangles'] = to_vector(triangles)
     data_dict['clustering'] = to_vector(clustering)
 
     df = pd.DataFrame(data_dict).set_index('node')
-    df.to_csv(os.path.join(root_dir, '.\data\graph\vertex_metrics.csv'))
+    df.to_csv(os.path.join(root_dir, 'data', 'graph', 'vertex_metrics.csv'))
 
 def main():
+    # calculate_metrics()
     communities = nx.community.louvain_communities(G, weight='weight', resolution=3, seed=72)
-    average_path_length_unweighted = nx.average_shortest_path_length(G)
-    print(f"Unweighted path length: {average_path_length_unweighted}")
-    
     communities_filename = os.path.join(root_dir, '.\data\graph\general_metrics.txt')
+
+    node_to_community = dict()
+    node_colors = []
+
     with open(communities_filename, 'w', encoding='utf-8') as f:
-        print("Communities:", file=f)
+        # adicionar informações sobre as comunidades ao arquivo
+        modularity = nx.community.modularity(G, communities, weight='weight')
+        print(f"Communities: Modularity = {modularity}", file=f)
         for index, community in enumerate(communities):
             print(index, file=f)
             print(community, file=f)
             print(get_info_by_node(community, ['genre', 'second_genre', 'artist','descriptor']), end='\n\n', file=f)
 
-    node_to_community = dict()
-    node_colors = []
-    for index, community in enumerate(communities):
-        for vertex in community:
-            node_to_community[vertex] = index
-            node_colors.append(index)
+            # colore os vértices com base na comunidade
+            for vertex in community: # col
+                node_to_community[vertex] = index
+                node_colors.append(index)
 
     pos = nx.spring_layout(G, weight='weight', seed=7, k=0.33, iterations=100)
     nx.draw_networkx_nodes(G, pos=pos, node_size=4, node_color=node_colors)
